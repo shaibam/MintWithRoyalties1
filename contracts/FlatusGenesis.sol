e3f1c4f054ceac1d35028a0ef4cbbfdb669386b1
shai kimchi <shai.k@diglobal.tech>
2022-04-21 01:24:46 +0300

    Openzepplin defender

--- contracts/FlatusGenesis.sol
+++ contracts/FlatusGenesis.sol
@@ -1,32 +1,38 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
-import '@openzeppelin/contracts/utils/Counters.sol';
-import '@openzeppelin/contracts/access/Ownable.sol'; 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-import './ERC2981ContractWideRoyalties.sol';
+import "./ERC2981ContractWideRoyalties.sol";
 
 /// @title Flatus-Genesis NFT mitifier
 
 contract FlatusGenesis is
     ERC721URIStorage,
-    // ERC721,
+    Pausable,
+    ReentrancyGuard,
     Ownable,
     ERC2981ContractWideRoyalties
 {
-
     //configuration
+    // Address of OZ Defender's Relayer
+    address private immutable _defender;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     string baseURI;
     string public baseExtension = ".json";
     uint256 nextTokenId;
 
-    constructor()
-        ERC721("Flatus", "Genesis")
-    {}
+    constructor(address defender) ERC721("FlatusGenesisNFT", "FGNFT") {
+        require(defender != address(0));
+        _defender = defender;
+    }
 
     /// @inheritdoc	ERC165
     function supportsInterface(bytes4 interfaceId)
@@ -43,24 +49,47 @@ contract FlatusGenesis is
     /// @dev This function in a real contract should be protected with a onlyOwner (or equivalent) modifier
     /// @param recipient the royalties recipient
     /// @param value percentage (using 2 decimals - 10000 = 100, 0 = 0)
-    function setRoyalties(address recipient, uint256 value) public onlyOwner{
+    function setRoyalties(address recipient, uint256 value) public onlyOwner {
         _setRoyalties(recipient, value);
     }
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public
-        payable
-        virtual
-        returns (uint256)
-    {
+    function mintNFT(
+        address recipient,
+        string memory tokenURI,
+        bytes32 hash,
+        bytes memory signature
+    ) public payable virtual returns (uint256) {
+        uint256 tokenId = _tokenIds.current();
+
+        require(
+            hash == keccak256(abi.encode(msg.sender, tokenId, address(this))),
+            "Invalid hash"
+        );
+
+        require(
+            ECDSA.recover(ECDSA.toEthSignedMessageHash(hash), signature) ==
+                _defender,
+            "Invalid signature"
+        );
+
         require(msg.value >= 20, "Not enough ETH sent; check price!"); // requests payment prior to minting
 
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
-        _safeMint(recipient, newItemId);     
-        _setTokenURI(newItemId, tokenURI);        
-    
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
         return newItemId;
-    }    
+    }
+
+    function pause() external {
+        require(msg.sender == _defender, "Unauthorized");
+        _pause();
+    }
+
+    function unpause() external {
+        require(msg.sender == _defender, "Unauthorized");
+        _unpause();
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,16 @@
+const { RELAYER_ADDRESS } = process.env;
 async function main() {
-    // Grab the contract factory 
-    const MyNFT = await ethers.getContractFactory("FlatusGenesis");
- 
-    // Start deployment, returning a promise that resolves to a contract object
-    const myNFT = await MyNFT.deploy(); // Instance of the contract 
-    console.log("Contract deployed to address:", myNFT.address);
- }
- 
- main()
-   .then(() => process.exit(0))
-   .catch(error => {
-     console.error(error);
-     process.exit(1);
-   });
+  // Grab the contract factory 
+  const MyNFT = await ethers.getContractFactory("FlatusGenesis");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy(RELAYER_ADDRESS); // Instance of the contract 
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
