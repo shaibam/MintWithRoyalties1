e7e8985c0b65670166aba6896b2053f0337dbb20
shai kimchi <shai.k@diglobal.tech>
2022-04-20 23:42:05 +0300

    deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,14 +5,16 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 
+console.log({ API_URL, PRIVATE_KEY });
+
 module.exports = {
-   solidity: "0.8.0",
-   defaultNetwork: "rinkeby",
-   networks: {
-      hardhat: {},
-      rinkeby: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
-      }
-   },
+  solidity: "0.8.1",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
 }
